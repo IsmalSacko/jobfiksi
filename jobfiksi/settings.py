da192ce58d29bcf1f9747fcb13cde691361e858d
Ismaila SACKO <ismalsacko@yahoo.fr>
2024-10-21 22:04:05 +0200

    deploiement

--- jobfiksi/settings.py
+++ jobfiksi/settings.py
@@ -114,10 +114,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
