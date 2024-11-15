c4440ebe8d4f0f860cecbfbb2d86d4ddfe55c537
Ismaila SACKO <ismalsacko@yahoo.fr>
2024-11-15 11:01:23 +0100

    Mise à jour de la base de données

--- jobfiksi/settings.py
+++ jobfiksi/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default-secret-key')
 DEBUG = True
 
-CORS_ORIGIN_ALLOW_ALL = True
+
 ALLOWED_HOSTS = ['*']
 
 
