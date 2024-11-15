d40fdcf6b6c57ac018be91c63a84895ee8ee8f48
Ismaila SACKO <ismalsacko@yahoo.fr>
2024-11-15 10:57:18 +0100

    Mise à jour de la base de données

--- jobfiksi/settings.py
+++ jobfiksi/settings.py
@@ -6,7 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default-secret-key')
 DEBUG = True
 
+CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['*']
+
+
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -84,6 +88,7 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
@@ -119,7 +124,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-CORS_ALLOW_CREDENTIALS = True  # Permet l'utilisation des cookies et de l'authentification
 
 
