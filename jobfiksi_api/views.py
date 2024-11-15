01ed17b955581e89c8509198abf04c6c1c29bf8f
Ismaila SACKO <ismalsacko@yahoo.fr>
2024-11-15 11:39:47 +0100

    Mise à jour de la base de données

--- jobfiksi_api/views.py
+++ jobfiksi_api/views.py
@@ -91,7 +91,7 @@ class UserListCreateRetrieveView(generics.ListCreateAPIView):
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserCreateSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
     def retrieve(self, request, *args, **kwargs):
         user = self.get_object()
