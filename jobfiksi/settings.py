83f4a5f088814cbac11f92c20851f2b4b88d4d42
Ismaila SACKO <ismalsacko@yahoo.fr>
2024-11-16 05:58:43 +0100

    Mise à jour de la base de données

--- jobfiksi/settings.py
+++ jobfiksi/settings.py
@@ -119,3 +119,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
+
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+    'x-csrftoken',
+]
