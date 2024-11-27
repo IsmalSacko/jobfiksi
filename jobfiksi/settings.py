54e63de1aea2deea8d59ad4d12423b6284aa0b64
Ismaila SACKO <ismalsacko@yahoo.fr>
2024-11-27 09:34:50 +0100

    Modif du settings.py

--- jobfiksi/settings.py
+++ jobfiksi/settings.py
@@ -54,32 +54,32 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'jobfiksi.wsgi.application'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jobfiksi',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        },
-    }
-}
 #
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'jobfiksi',
-#         'USER': 'ismael_admin',
-#         'PASSWORD': 'sacko&diaby',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
 #         'HOST': 'localhost',
 #         'PORT': '3306',
-#
+#         'OPTIONS': {
+#             'charset': 'utf8mb4',
+#         },
 #     }
 # }
+#
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jobfiksi',
+        'USER': 'ismael_admin',
+        'PASSWORD': 'sacko&diaby',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
+    }
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
