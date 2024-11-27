6c52845cc4807cf2fc44a31d9741afd17e8156b5
Ismaila SACKO <ismalsacko@yahoo.fr>
2024-11-27 20:34:01 +0100

    Modif du settings.py

--- jobfiksi_api/serializers.py
+++ jobfiksi_api/serializers.py
@@ -77,54 +77,6 @@ class CandidatSerializer(serializers.ModelSerializer):
 
         if 'image' in validated_data and validated_data['image'] is not None:
             instance.image = validated_data['image']
-        if 'nom' in validated_data and validated_data['nom'] is not None:
-            instance.nom = validated_data['nom']
-        if 'prenom' in validated_data and validated_data['prenom'] is not None:
-            instance.prenom = validated_data['prenom']
-        if 'tel' in validated_data and validated_data['tel'] is not None:
-            instance.tel = validated_data['tel']
-        if 'date_naissance' in validated_data and validated_data['date_naissance'] is not None:
-            instance.date_naissance = validated_data['date_naissance']
-        if 'genre' in validated_data and validated_data['genre'] is not None:
-            instance.genre = validated_data['genre']
-        if 'ville' in validated_data and validated_data['ville'] is not None:
-            instance.ville = validated_data['ville']
-        if 'num_et_rue' in validated_data and validated_data['num_et_rue'] is not None:
-            instance.num_et_rue = validated_data['num_et_rue']
-        if 'code_postal' in validated_data and validated_data['code_postal'] is not None:
-            instance.code_postal = validated_data['code_postal']
-        if 'pays' in validated_data and validated_data['pays'] is not None:
-            instance.pays = validated_data['pays']
-        if 'iban' in validated_data and validated_data['iban'] is not None:
-            instance.iban = validated_data['iban']
-        if 'secu_sociale' in validated_data and validated_data['secu_sociale'] is not None:
-            instance.secu_sociale = validated_data['secu_sociale']
-        if 'disponibilite' in validated_data and validated_data['disponibilite'] is not None:
-            instance.disponibilite = validated_data['disponibilite']
-        if 'plage_horaire' in validated_data and validated_data['plage_horaire'] is not None:
-            instance.plage_horaire = validated_data['plage_horaire']
-        if 'niveau_etude' in validated_data and validated_data['niveau_etude'] is not None:
-            instance.niveau_etude = validated_data['niveau_etude']
-        if 'experience' in validated_data and validated_data['experience'] is not None:
-            instance.experience = validated_data['experience']
-        if 'compentence' in validated_data and validated_data['compentence'] is not None:
-            instance.compentence = validated_data['compentence']
-        if 'formation' in validated_data and validated_data['formation'] is not None:
-            instance.formation = validated_data['formation']
-        if 'etablissement' in validated_data and validated_data['etablissement'] is not None:
-            instance.etablissement = validated_data['etablissement']
-        if 'date_debut' in validated_data and validated_data['date_debut'] is not None:
-            instance.date_debut = validated_data['date_debut']
-        if 'date_fin' in validated_data and validated_data['date_fin'] is not None:
-            instance.date_fin = validated_data['date_fin']
-        if 'preference_salaire' in validated_data and validated_data['preference_salaire'] is not None:
-            instance.preference_salaire = validated_data['preference_salaire']
-        if 'salaire_min' in validated_data and validated_data['salaire_min'] is not None:
-            instance.salaire_min = validated_data['salaire_min']
-        if 'salaire_max' in validated_data and validated_data['salaire_max'] is not None:
-            instance.salaire_max = validated_data['salaire_max']
-        if 'notification_mail' in validated_data and validated_data['notification_mail'] is not None:
-            instance.notification_mail = validated_data['notification_mail']
 
         # Mise à jour des autres champs
         for field in [
@@ -167,29 +119,10 @@ class RestaurantSerializer(serializers.ModelSerializer):
         ]
 
     def update(self, instance, validated_data):
-        # Mise à jour de l'image uniquement si un fichier est fourni dans validated_data
-        if 'image' in validated_data and validated_data['image'] is not None:
-            instance.image = validated_data['image']
-        if 'email_pro' in validated_data and validated_data['email_pro'] is not None:
-            instance.email_pro = validated_data['email_pro']
-        if 'tel' in validated_data and validated_data['tel'] is not None:
-            instance.tel = validated_data['tel']
-        if 'ville' in validated_data and validated_data['ville'] is not None:
-            instance.ville = validated_data['ville']
-        if 'num_et_rue' in validated_data and validated_data['num_et_rue'] is not None:
-            instance.num_et_rue = validated_data['num_et_rue']
-        if 'code_postal' in validated_data and validated_data['code_postal'] is not None:
-            instance.code_postal = validated_data['code_postal']
-        if 'pays' in validated_data and validated_data['pays'] is not None:
-            instance.pays = validated_data['pays']
-        if 'site_web' in validated_data and validated_data['site_web'] is not None:
-            instance.site_web = validated_data['site_web']
-        if 'notification_mail' in validated_data and validated_data['notification_mail'] is not None:
-            instance.notification_mail = validated_data['notification_mail']
-
-        # Mise à jour des autres champs
+        # Mise à jour des autres champs et garder les anciennes valeurs si non fournies
         for field in [
-            'nom', 'email_pro', 'tel', 'ville', 'num_et_rue', 'code_postal', 'pays', 'site_web', 'notification_mail'
+            'nom', 'email_pro', 'tel', 'ville', 'image', 'num_et_rue', 'ville', 'code_postal', 'pays', 'site_web',
+            'notification_mail'
         ]:
             setattr(instance, field, validated_data.get(field, getattr(instance, field)))
 
