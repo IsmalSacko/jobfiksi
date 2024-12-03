d2066339a10735a8c2ab7d4445dbd3717b911998
Ismaila SACKO <ismalsacko@yahoo.fr>
2024-12-03 11:49:53 +0100

    Fonctionnalités d'envoi de mail de confirmation

--- jobfiksi_api/apps.py
+++ jobfiksi_api/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class JobfiksiApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'jobfiksi_api'
-
-    def ready(self):
-        import jobfiksi_api.signals  # Importez les signaux ici
